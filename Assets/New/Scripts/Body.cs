a7fb64198f137c95fe5eb680ae5498a018af4312
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2022-05-02 18:19:03 +0300

    clear trash

--- Assets/New/Scripts/Body.cs
+++ Assets/New/Scripts/Body.cs
@@ -5,9 +5,6 @@ namespace New
 {
     public class Body : MonoBehaviour, IHitable
     {
-        [HideInInspector]
-        public BodyUpSystem upSystem;
-        
         public virtual void GetHit(Vector3 hitPoint)
         {
             Debug.Log(name + " GetHit");
